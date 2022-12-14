1fb8d7b44417bd0797f516cb9bd49ff5896f3f19
Poscidonio <guilhermeposcidonio@gmail.com>
2022-12-14 17:54:51 -0300

    Continuando Projeto nestJS.

--- api-smartranking/src/jogadores/jogadores.service.ts
+++ api-smartranking/src/jogadores/jogadores.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 
@@ -10,5 +11,25 @@ export class JogadoresService {
 
   async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto) {
     this.logger.log(`criaJogadorDto: ${criarJogadorDto}`);
+    this.criar(criarJogadorDto);
+  }
+
+  async consultarTodosJogadores(): Promise<Jogador[]> {
+    return await this.jogadores;
+  }
+
+  private criar(criaJogadorDto: CriarJogadorDto): void {
+    const { nome, telefoneCelular, email } = criaJogadorDto;
+
+    const jogador: Jogador = {
+      _id: uuidv4(),
+      nome,
+      telefoneCelular,
+      email,
+      ranking: 'A',
+      posicaoRanking: 1,
+      urlFotoJogador: 'www.google.com.br/foto123.jpg',
+    };
+    this.jogadores.push(jogador);
   }
 }
