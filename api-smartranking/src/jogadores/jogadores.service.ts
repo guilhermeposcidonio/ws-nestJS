0c1f381c436728439b49f7fae018412ea5a59d4c
Poscidonio <guilhermeposcidonio@gmail.com>
2022-12-13 22:30:23 -0300

    Iniciando com o nest

---
+++ api-smartranking/src/jogadores/jogadores.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+
+@Injectable()
+export class JogadoresService {
+  private jogadores: Jogador[] = [];
+
+  private readonly logger = new Logger(JogadoresService.name);
+
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto) {
+    this.logger.log(`criaJogadorDto: ${criarJogadorDto}`);
+  }
+}
